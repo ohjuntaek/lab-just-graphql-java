affa176f752ca2b7d11e860d0889972b5fcad020
ohjuntaek <201211734@pukyong.ac.kr>
2020-07-27 23:43:25 +0900

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+
+[노션 번역 낙서](https://www.notion.so/jundev/GraphQL-java-docs-3dac60468d5445818842c1824d7ce9f7)
---
+++ src/main/java/me/example/labjustgraphqljava/LabJustGraphqlJavaApplication.java
@@ -0,0 +1,13 @@
+package me.example.labjustgraphqljava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabJustGraphqlJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabJustGraphqlJavaApplication.class, args);
+    }
+
+}
