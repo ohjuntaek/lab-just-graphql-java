6ec25b52bbb8724f083fad3bdc2cb2807338e47a
ohjuntaek <201211734@pukyong.ac.kr>
2020-07-31 00:19:33 +0900

    로컬 h2 연동

--- src/test/java/me/example/labjustgraphqljava/domain/FoodRepositoryTest.java
+++ src/test/java/me/example/labjustgraphqljava/domain/FoodRepositoryTest.java
@@ -1,16 +1,17 @@
 package me.example.labjustgraphqljava.domain;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class FoodRepositoryTest {
     @Autowired FoodRepository repository;
 
     @Test
+//    @Commit
+    @Disabled
     void saveFoodTest() {
         repository.save(Food.builder().name("닭고기").build());
     }
